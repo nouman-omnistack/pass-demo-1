8160f566e55b865d2b6b0fce04d6e1d654fd1964
Nouman <nouman@omnistack.co>
2026-02-17 13:15:45 +0500

    Dev Code 7

---
+++ app/pass/callback/route.ts
@@ -0,0 +1,16 @@
+// 'use server';
+import { NextRequest, NextResponse } from 'next/server';
+
+//export async function GET(request: NextRequest) {
+export async function POST(req: NextRequest) {
+console.log('here');
+    const formData = await req.formData();
+    const res_data = Object.fromEntries(formData);
+
+    return new Response(JSON.stringify({ response_data: res_data }), {
+    status: 200,
+    headers: { 'content-type': 'application/json' },
+    });
+}
+
+
