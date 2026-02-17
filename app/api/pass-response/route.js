6036f512f07073c346ec169dcaa74b856019480a
Nouman <nouman@omnistack.co>
2026-02-17 12:55:44 +0500

    Dev Code 5

--- app/api/pass-response/route.js
+++ app/api/pass-response/route.js
@@ -2,40 +2,51 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request) {
+import { cookies } from 'next/headers';
 
-  try {
-  
-    if (!request) {
-      return NextResponse.json('Invalid request sent to server . . . ', { status: 400 });
-    }
+export async function POST(req) {
+  const formData = await req.formData();
+  const res_data = Object.fromEntries(formData);
 
-    const contentType = request.headers.get('content-type');
+  return new Response(JSON.stringify({ response_data: res_data }), {
+    status: 200,
+    headers: { 'content-type': 'application/json' },
+  });
+}
+// export async function GET(request) {
+
+//   try {
   
-    let body;
-    if (contentType?.includes('application/json')) {
-      body = await request.json();
-    } else if (contentType?.includes('application/x-www-form-urlencoded')) {
-      body = Object.fromEntries((await request.formData()).entries());
-      console.log(' >>> wokring by application/x-www-form-urlencoded . . . ');
-    } else if (contentType?.includes('multipart/form-data')) {
-      body = await request.formData();
-    } else {
-      body = await request.text();
-    }
+//     if (!request) {
+//       return NextResponse.json('Invalid request sent to server . . . ', { status: 400 });
+//     }
+
+//     const contentType = request.headers.get('content-type');
+  
+//     let body;
+//     if (contentType?.includes('application/json')) {
+//       body = await request.json();
+//     } else if (contentType?.includes('application/x-www-form-urlencoded')) {
+//       body = Object.fromEntries((await request.formData()).entries());
+//       console.log(' >>> wokring by application/x-www-form-urlencoded . . . ');
+//     } else if (contentType?.includes('multipart/form-data')) {
+//       body = await request.formData();
+//     } else {
+//       body = await request.text();
+//     }
 
     
-    if (body.res_cd == '0000') {
-      return NextResponse.json({ ok: true, res_cd: body.res_cd, res_msg: body.res_msg, response: body });
-    } else {
-      return NextResponse.json({ ok: false, res_cd: body.res_cd, res_msg: body.res_msg, response: body });
-    }
+//     if (body.res_cd == '0000') {
+//       return NextResponse.json({ ok: true, res_cd: body.res_cd, res_msg: body.res_msg, response: body });
+//     } else {
+//       return NextResponse.json({ ok: false, res_cd: body.res_cd, res_msg: body.res_msg, response: body });
+//     }
 
-  } catch (error) {
-    console.error('Error:', error?.response?.data || error?.message)
-    return NextResponse.json({api_res_msg: 'An error occurred while verifying ownership'}, { status: 500 });
-  }
-}
+//   } catch (error) {
+//     console.error('Error:', error?.response?.data || error?.message)
+//     return NextResponse.json({api_res_msg: 'An error occurred while verifying ownership'}, { status: 500 });
+//   }
+// }
 
 
 // export async function GET(request) {
