c12808dac6037e8c8f0e9186f4d2a75236902d67
Nouman <nouman@omnistack.co>
2026-02-17 12:09:58 +0500

    Dev Code 2

--- app/testbutton/page.tsx
+++ app/testbutton/page.tsx
@@ -7,28 +7,28 @@ import { useState, useEffect } from 'react';
 
 export default function TestPage() {
 
-const [orderID, setOrderID] = useState(0);
+const [orderID, setOrderID] = useState("");
 
   useEffect(() => {
       
-      if (orderID !== 0) return;
+      if (orderID !== "") return;
 
-      var today = new Date();
-      var year  = today.getFullYear();
-      var month = today.getMonth() + 1;
-      var date  = today.getDate();
-      var time  = today.getTime();
+      const today = new Date();
+      const year  = today.getFullYear();
+      let month = String(today.getMonth() + 1);
+      const date  = today.getDate();
+      const time  = today.getTime();
 
       if (parseInt(month) < 10){
           month = "0" + month;
       }
-      var test_order_id = year + "" + month + "" + date + "" + time;
+      const test_order_id = year + "" + month + "" + date + "" + time;
       setOrderID(test_order_id);
   }, [orderID]);
 
   return (
     <>
-      {(orderID != 0) && (
+      {(orderID != "") && (
         <div style={{ padding: '20px', textAlign: 'center' }}>
           <PASSButtonLink 
             href="#" 
