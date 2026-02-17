8366e7773203459adc2ee82bfe547c862a296ad4
Nouman <nouman@omnistack.co>
2026-02-17 13:33:06 +0500

    Dev Code 9

--- app/pass/callback/route.ts
+++ app/pass/callback/route.ts
@@ -1,19 +1,15 @@
 // 'use server';
 import { NextRequest, NextResponse } from 'next/server';
 
-//export async function GET(request: NextRequest) {
 export async function GET(req: NextRequest) {
-console.log('here-GET');
-    const formData = await req.formData();
-    const res_data = Object.fromEntries(formData);
+    console.log('here-GET');
+    const { searchParams } = new URL(req.url);
 
-    return new Response(JSON.stringify({ response_data: res_data }), {
-    status: 200,
-    headers: { 'content-type': 'application/json' },
-    });
+    const responseData = Object.fromEntries(searchParams.entries());
+
+    return NextResponse.json({ response_data: responseData });
 }
 
-//export async function GET(request: NextRequest) {
 export async function POST(req: NextRequest) {
 console.log('here-POST');
     const formData = await req.formData();
